a971b5979011c7b4b4580e09d7b77614614aa45e
Greg Szabo <greg@philosobear.com>
2017-08-31 03:13:23 -0400

    Initial commit

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -5,17 +5,18 @@ import (
 	"strings"
 	"strconv"
 	"time"
+	"math"
 )
 
 type data struct {
-	User    uint64            `json:"user"`
-	Nice    uint64            `json:"nice"`
-	System  uint64        `json:"system"`
-	Idle    uint64            `json:"idle"`
-	IOWait  uint64        `json:"iowait"`
-	IRQ     uint64            `json:"irq"`
-	SoftIRQ uint64        `json:"softirq"`
-	Total   uint64        `json:"total"`
+	User    uint64		`json:"user"`
+	Nice    uint64		`json:"nice"`
+	System  uint64		`json:"system"`
+	Idle    uint64		`json:"idle"`
+	IOWait  uint64		`json:"iowait"`
+	IRQ     uint64		`json:"irq"`
+	SoftIRQ uint64		`json:"softirq"`
+	Total   uint64		`json:"total"`
 }
 
 type stat struct {
@@ -24,21 +25,24 @@ type stat struct {
 	System float32	`json:"system"`
 	Idle float32	`json:"idle"`
 	IOWait float32	`json:"iowait"`
-	IRQ float32	`json:"irq"`
+	IRQ float32		`json:"irq"`
 	SoftIRQ float32	`json:"softirq"`
 }
 
 var (
 	dataStore data
 	statStore stat
+	timeFrame int
+	exp float32
 )
 
 func init() {
+	dataStore = getData()
+	SetTimeFrame(5)
 	go startMonitor()
 }
 
 func startMonitor() {
-	dataStore = getData()
 	for {
 		newData := getData()
 		statStore = getStat(dataStore, newData)
@@ -49,6 +53,21 @@ func startMonitor() {
 
 func Stat()(stat) {
 	return statStore
+}
+
+func TimeFrame() int {
+	return timeFrame
+}
+
+func SetTimeFrame(t int) {
+	if t < 0 {
+		t = 0
+	}
+	if t > 120 {
+		t = 120
+	}
+	timeFrame = t
+	exp = (float32)(math.Exp(-0.016666666666667 / (float64)(timeFrame)))
 }
 
 func getData() (output data) {
@@ -86,13 +105,20 @@ func getStat(old, new data) (output stat) {
 	if div == 0 {
 		div = 1
 	}
+	QUser := 100. * (float32)(new.User - old.User) / (float32)(div)
+	QNice := 100. * (float32)(new.Nice - old.Nice) / (float32)(div)
+	QSystem := 100. * (float32)(new.System - old.System) / (float32)(div)
+	QIdle := 100. * (float32)(new.Idle - old.Idle) / (float32)(div)
+	QIOWait := 100. * (float32)(new.IOWait - old.IOWait) / (float32)(div)
+	QIRQ := 100. * (float32)(new.IRQ - old.IRQ) / (float32)(div)
+	QSoftIRQ := 100. * (float32)(new.SoftIRQ - old.SoftIRQ) / (float32)(div)
 	return stat{
-		100. * (float32)(new.User - old.User) / (float32)(div),
-		100. * (float32)(new.Nice - old.Nice) / (float32)(div),
-		100. * (float32)(new.System - old.System) / (float32)(div),
-		100. * (float32)(new.Idle - old.Idle) / (float32)(div),
-		100. * (float32)(new.IOWait - old.IOWait) / (float32)(div),
-		100. * (float32)(new.IRQ - old.IRQ) / (float32)(div),
-		100. * (float32)(new.SoftIRQ - old.SoftIRQ) / (float32)(div),
+		QUser + exp * (statStore.User - QUser),
+		QNice + exp * (statStore.Nice - QNice),
+		QSystem + exp * (statStore.System - QSystem),
+		QIdle + exp * (statStore.Idle - QIdle),
+		QIOWait + exp * (statStore.IOWait - QIOWait),
+		QIRQ + exp * (statStore.IRQ - QIRQ),
+		QSoftIRQ + exp * (statStore.SoftIRQ - QSoftIRQ),
 	}
 }
